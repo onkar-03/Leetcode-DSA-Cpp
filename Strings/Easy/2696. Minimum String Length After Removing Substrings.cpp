61f50bbc68658ad86be421d6632104baa2a9e185
Onkar Patel <onkarpatel10@gmail.com>
2024-10-07 22:48:44 +0530

    Added 2 more approaches to solve Minimum String Length after removing Substring

--- Strings/Easy/2696. Minimum String Length After Removing Substrings.cpp
+++ Strings/Easy/2696. Minimum String Length After Removing Substrings.cpp
@@ -1,6 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#include <string>
+
+class Solution
+{
+public:
+  int minLength(string s)
+  {
+    // Loop until no more "AB" or "CD" pairs can be found in the string
+    while (s.find("AB") != -1 || s.find("CD") != -1)
+    {
+      // Check if "AB" is present in the string
+      if (s.find("AB") != -1)
+      {
+        // Erase the first occurrence of "AB" from the string
+        s.erase(s.find("AB"), 2); // Removes 2 char starting from the position of "AB"
+      }
+      // Check if "CD" is present in the string
+      else if (s.find("CD") != -1)
+      {
+        // Erase the first occurrence of "CD" from the string
+        s.erase(s.find("CD"), 2); // Removes 2 char starting from the position of "CD"
+      }
+    }
+
+    // Return the length of the modified string after all pairs have been removed
+    return s.length();
+  }
+};
+
+// Approach-2 (Using stack)
+// T.C : O(n)
+// S.C : O(n)
+#include <stack>
+#include <string>
+
+class Solution
+{
+public:
+  int minLength(string s)
+  {
+    // Create a stack to keep track of characters
+    stack<char> st;
+
+    // Iterate through each character in the string
+    for (int i = 0; i < s.length(); i++)
+    {
+      char currentChar = s[i]; // Get the current character
+
+      // If the stack is empty, push the current character onto the stack
+      if (st.empty())
+      {
+        st.push(currentChar);
+        continue; // Move to the next character
+      }
+
+      // Check if the current character and the top of the stack form a removable pair "AB"
+      if (currentChar == 'B' && st.top() == 'A')
+      {
+        // If they do, pop the top character from the stack
+        st.pop(); // Removes 'A'
+      }
+      // Check if the current character and the top of the stack form a removable pair "CD"
+      else if (currentChar == 'D' && st.top() == 'C')
+      {
+        // If they do, pop the top character from the stack
+        st.pop();
+      }
+      else
+      {
+        // If no pair is found, push the current character onto the stack
+        st.push(currentChar);
+      }
+    }
+
+    // The size of the stack now represents the length of the modified string
+    return st.size(); // Return the number of valid characters remaining
+  }
+};
+
 // Approach-3 (Using 2 pointers = i : read, j ; write)
 // T.C : O(n)
 // S.C : O(1)
