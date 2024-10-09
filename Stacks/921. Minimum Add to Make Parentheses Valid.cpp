88d18b836ce9614fd1916d4f779c0fb8026bd317
Onkar Patel <onkarpatel10@gmail.com>
2024-10-09 18:52:25 +0530

    Added Approach 2 for min value to make parenthesis valid

--- Stacks/921. Minimum Add to Make Parentheses Valid.cpp
+++ Stacks/921. Minimum Add to Make Parentheses Valid.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach-1 (Using stack)
+// T.C : O(n)
+// S.C : O(n)
 class Solution
 {
 public:
@@ -39,5 +42,51 @@ public:
     // At the end, the stack will still contain any unmatched '(' parentheses, and 'open' will contain the number of unmatched ')' parentheses.
     // The sum of these two will give us the minimum number of parentheses to add to make the string valid.
     return open + st.size();
+  }
+};
+
+// Approach-2 (Without Using stack)
+// T.C : O(n)
+// S.C : O(1)
+class Solution
+{
+public:
+  // This function calculates the minimum number of parentheses to be added to make the string valid
+  int minAddToMakeValid(string s)
+  {
+    // 'size' keeps track of the number of unmatched opening parentheses '('
+    int size = 0;
+
+    // 'open' keeps track of the number of unmatched closing parentheses ')'
+    int open = 0;
+
+    // Loop through each character in the string 's'
+    for (char &ch : s)
+    {
+      // If the current character is an opening parenthesis '('
+      if (ch == '(')
+      {
+        // Increment 'size' because we have an unmatched '(' that needs to be closed
+        size++;
+      }
+      // If the current character is a closing parenthesis ')'
+      else if (size > 0)
+      {
+        // If there's an unmatched '(' (i.e., size > 0), we decrement 'size'
+        // because this ')' can balance a previous '('
+        size--;
+      }
+      else
+      {
+        // If there are no unmatched opening parentheses '(' to balance the current ')'
+        // Increment 'open' to keep track of unmatched closing parentheses ')'
+        open++;
+      }
+    }
+
+    // The total number of parentheses to add will be the sum of:
+    // 1. 'open': unmatched closing parentheses ')'
+    // 2. 'size': unmatched opening parentheses '('
+    return open + size;
   }
 };
