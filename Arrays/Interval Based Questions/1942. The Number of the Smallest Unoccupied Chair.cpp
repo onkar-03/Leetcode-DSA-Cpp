e3b2833feed0c8f01e2dfe3df65f9c25ff402ba1
Onkar Patel <onkarpatel10@gmail.com>
2024-10-12 00:20:48 +0530

    Added optimal Solution for Number of Smallest Unoccupied Chair

--- Arrays/Interval Based Questions/1942. The Number of the Smallest Unoccupied Chair.cpp
+++ Arrays/Interval Based Questions/1942. The Number of the Smallest Unoccupied Chair.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach-1 (Brute Force)
+// T.C : O(n^2)
+// S.C : O(n)
 class Solution
 {
 public:
@@ -43,5 +46,81 @@ public:
     }
     // Never executed if we know target friend is always assigned a chair
     return -1;
+  }
+};
+
+// Approach-2 (Using min-heaps)
+// T.C : O(nlogn)
+// S.C : O(n)
+class Solution
+{
+public:
+  int smallestChair(vector<vector<int>> &times, int targetFriend)
+  {
+    // Number of friends (based on the size of the input vector)
+    int n = times.size();
+
+    // Priority queue to track occupied chairs: (departure time, chair number).
+    // The priority queue will store pairs where the top element is the one with the earliest departure time, allowing us to free chairs as friends leave.
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> occupied;
+
+    // Priority queue to track free chairs: smallest chair number will be at the top (min-heap).
+    // This queue helps us allocate the smallest available chair number when assigning chairs.
+    priority_queue<int, vector<int>, greater<int>> free;
+
+    // Get the arrival time of the target friend to track when they arrive
+    int targetFriendArrivalTime = times[targetFriend][0];
+
+    // Variable to assign chair numbers sequentially as new chairs are needed.
+    int chairNumber = 0;
+
+    // Sort the 'times' array based on arrival times so that we can allocate chairs in the correct order.
+    sort(times.begin(), times.end());
+
+    // Iterate through each friend's arrival and departure times
+    for (int i = 0; i < n; i++)
+    {
+      int arrival = times[i][0];   // Friend's arrival time
+      int departure = times[i][1]; // Friend's departure time
+
+      // Free up any chairs where the friend has already departed by the current friend's arrival time.
+      // We check the top of the 'occupied' queue, and if the top chair's departure time is earlier or equal to the current arrival, we push it back to the 'free' queue since it becomes available.
+      while (!occupied.empty() && occupied.top().first <= arrival)
+      {
+        // Add the chair back to the available free queue
+        free.push(occupied.top().second);
+        // Remove it from the occupied queue
+        occupied.pop();
+      }
+
+      // If no chairs are free, assign a new chair number.
+      while (free.empty())
+      {
+        // No chairs available in 'free', so assign a new chair and increment chairNumber
+        occupied.push({departure, chairNumber++});
+
+        // If this friend is the target friend, return the assigned chair number.
+        if (arrival == targetFriendArrivalTime)
+        {
+          // Return the current chair number
+          return chairNumber;
+        }
+      }
+
+      // If there are free chairs, we allocate the smallest available chair.
+      int leastChairAvailable = free.top();
+      free.pop(); // Remove the chair from the available queue
+
+      // If the current friend is the target friend, return the chair number they're assigned.
+      if (arrival == targetFriendArrivalTime)
+      {
+        return leastChairAvailable;
+      }
+
+      // Assign the available chair to the current friend and mark it as occupied until their departure.
+      occupied.push({departure, leastChairAvailable});
+    }
+
+    return -1; // This line should never be reached if input guarantees a valid answer.
   }
 };
