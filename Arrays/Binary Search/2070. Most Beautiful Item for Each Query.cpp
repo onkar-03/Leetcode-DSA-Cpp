2b1cc9353be3c26890269dd9f99234c30c310032
Onkar Patel <onkarpatel10@gmail.com>
2024-11-13 23:48:21 +0530

    Solved Count the Number of Fair Pairs

--- Arrays/Binary Search/2070. Most Beautiful Item for Each Query.cpp
+++ Arrays/Binary Search/2070. Most Beautiful Item for Each Query.cpp
@@ -32,15 +32,16 @@ public:
       }
     }
 
+    // Return the final maxBeauty calculated
     return maxBeauty;
   }
 
   // Main function to process items and queries to return maximum beauty for each query price
   vector<int> maximumBeauty(vector<vector<int>> &items, vector<int> &queries)
   {
-    int n = items.size();
-    int m = queries.size();
-    vector<int> result(m);
+    int n = items.size();   // Size of the items Array
+    int m = queries.size(); // Size of Query Array
+    vector<int> result(m);  // Result Array to store MaxBeauty for each query
 
     // Step 1: Sort items by price in ascending order for binary search
     // Format: {price, beauty}
@@ -48,7 +49,9 @@ public:
     sort(begin(items), end(items));
 
     // Step 2: Preprocess to update each item with the maximum beauty seen up to that price
-    int maxBeautySeen = items[0][1];
+    // So that we can get teh MaxBeauty at any place in the Array without computing it again
+    // int maxBeautySeen = items[0][1];
+    int maxBeautySeen = INT_MIN;
     for (int i = 1; i < n; i++) // O(n)
     {
       maxBeautySeen = max(maxBeautySeen, items[i][1]);
@@ -58,7 +61,10 @@ public:
     // Step 3: For each query, perform a binary search to find maximum beauty within the query price
     for (int i = 0; i < m; i++) // O(m)
     {
+      // For the specific Query  the maximum beauty of an item whose price is less than or equal to queries[j]
       int queryPrice = queries[i];
+
+      // Perform Binary Search to ge the last index of price that is <= query price and get its maxBeauty
       result[i] = customBinarySearch(items, queryPrice); // O(n logn)
     }
 
