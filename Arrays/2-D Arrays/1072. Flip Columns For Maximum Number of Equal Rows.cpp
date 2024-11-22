44d1b4257b41f20e3d74e22336e55e293f2ae85f
Onkar Patel <onkarpatel10@gmail.com>
2024-11-22 23:49:39 +0530

    Solved Flip Columns For Max Number of Equal Rows Brute Force

--- Arrays/2-D Arrays/1072. Flip Columns For Maximum Number of Equal Rows.cpp
+++ Arrays/2-D Arrays/1072. Flip Columns For Maximum Number of Equal Rows.cpp
@@ -1,6 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach-1 (Brute Force)
+// T.C : O(row^2 * col)
+// S.C : O(col)
+class Solution
+{
+public:
+  int maxEqualRowsAfterFlips(vector<vector<int>> &matrix)
+  {
+    int n = matrix[0].size();
+    int maxRows = 0;
+
+    for (auto &currRow : matrix)
+    {
+
+      vector<int> inverted(n);
+      int count = 0;
+
+      for (int col = 0; col < n; col++)
+      {
+        inverted[col] = currRow[col] == 0 ? 1 : 0;
+      }
+
+      for (auto &row : matrix)
+      {
+
+        if (row == currRow || row == inverted)
+        {
+          count++;
+        }
+      }
+
+      maxRows = max(maxRows, count);
+    }
+
+    return maxRows;
+  }
+};
+
 // Approach-2 (Using hashmap to store patterns)
 // T.C : O(row * col)
 // S.C : O(row*col)
