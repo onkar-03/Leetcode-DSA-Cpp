26be0ed471704db86fef65bf054ede61b8093a14
Onkar Patel <onkarpatel10@gmail.com>
2024-11-29 22:27:52 +0530

    Solved Min Time to visit Cell in a Grid usign Dijkstra Algo

--- Graph/2577. Minimum Time to Visit a Cell In a Grid.cpp
+++ Graph/2577. Minimum Time to Visit a Cell In a Grid.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Approach (Using Dijkstra's Algorithm) - STORING RESULT IN 2D Matrix
+// Approach - 1 (Using Dijkstra's Algorithm) - STORING RESULT IN 2D Matrix
 // T.C : O(m*n log(m*n))
 // S.C : O(m*n)
 class Solution
@@ -64,6 +64,64 @@ public:
           pq.push({grid[r][c], {r, c}});
           result[r][c] = grid[r][c];
         }
+      }
+    }
+    return -1;
+  }
+};
+
+// Approach-2 (Using Dijkstra's Algorithm) - Without Storing result in a 2D Matrix
+// T.C : O(m*n log(m*n))
+// S.C : O(m*n)
+class Solution
+{
+public:
+  vector<vector<int>> direction{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+#define P pair<int, pair<int, int>>
+
+  int minimumTime(vector<vector<int>> &grid)
+  {
+    int m = grid.size();
+    int n = grid[0].size();
+
+    if (grid[0][1] > 1 && grid[1][0] > 1)
+      return -1;
+
+    vector<vector<bool>> visited(m, vector<bool>(n, false));
+    priority_queue<P, vector<P>, greater<P>> pq;
+
+    pq.push({grid[0][0], {0, 0}}); // Start at top-left corner
+    while (!pq.empty())
+    {
+      // Get the current time, row, and column
+      int time = pq.top().first;
+      int row = pq.top().second.first;
+      int col = pq.top().second.second;
+      pq.pop();
+
+      // Reached destination
+      if (row == m - 1 && col == n - 1)
+        return time;
+
+      // Mark the current cell as visited
+      if (visited[row][col])
+        continue;
+      visited[row][col] = true;
+
+      // Explore the neighboring cells
+      for (auto dir : direction)
+      {
+        int r = row + dir[0];
+        int c = col + dir[1];
+        if (r < 0 || r >= m || c < 0 || c >= n || visited[r][c])
+          continue;
+
+        if (grid[r][c] <= time + 1)
+          pq.push({time + 1, {r, c}});
+        else if ((grid[r][c] - time) % 2 == 0)
+          pq.push({grid[r][c] + 1, {r, c}});
+        else
+          pq.push({grid[r][c], {r, c}});
       }
     }
     return -1;
