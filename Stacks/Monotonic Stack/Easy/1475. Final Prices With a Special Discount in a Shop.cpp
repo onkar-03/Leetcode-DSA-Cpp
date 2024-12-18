4b98e92da8765beeae18f67e127e32b3578d7f58
Onkar Patel <onkarpatel10@gmail.com>
2024-12-18 17:45:17 +0530

    Solved Using Monotonic Stack Final Prices after Discount

--- Stacks/Monotonic Stack/Easy/1475. Final Prices With a Special Discount in a Shop.cpp
+++ Stacks/Monotonic Stack/Easy/1475. Final Prices With a Special Discount in a Shop.cpp
@@ -8,7 +8,7 @@ class Solution
 public:
   vector<int> finalPrices(vector<int> &prices)
   {
-    // Size of Prices Vwector
+    // Size of Prices Vector
     int n = prices.size();
 
     // Initialize the result vector as a copy of the input prices.
@@ -28,6 +28,46 @@ public:
           break;                  // Stop searching after finding the first valid discount.
         }
       }
+    }
+
+    // Return the final array of prices after applying discounts.
+    return result;
+  }
+};
+
+// Approach-2 (Monotonic Stack)
+// Time Complexity: O(2 * n) ~= O(n)
+//    - Each element is pushed and popped from the stack at most once.
+// Space Complexity: O(n) Due to the stack that stores indices.
+class Solution
+{
+public:
+  vector<int> finalPrices(vector<int> &prices)
+  {
+    // Size of prices
+    int n = prices.size();
+
+    // Initialize the result vector as a copy of the input prices.
+    vector<int> result(begin(prices), end(prices));
+
+    // Stack to maintain indices of prices in a monotonic decreasing order.
+    stack<int> st;
+
+    // Iterate through the prices array.
+    for (int i = 0; i < n; i++)
+    {
+
+      // While the stack is not empty and the current price is smaller than  or equal to the price at the index stored at the top of the stack:
+      while (!st.empty() && prices[i] <= prices[st.top()])
+      {
+        // Apply the discount: Subtract the current price from the price at the index on top of the stack.
+        result[st.top()] -= prices[i];
+        // Pop the index from the stack since the discount has been applied.
+        st.pop();
+      }
+
+      // Push the current index onto the stack.
+      st.push(i);
     }
 
     // Return the final array of prices after applying discounts.
