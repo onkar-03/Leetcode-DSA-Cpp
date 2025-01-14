3ec13d7e6e9e4b9db53580519d2dba92d9e6706c
Onkar Patel <onkarpatel10@gmail.com>
2025-01-14 23:43:41 +0530

    Solved using Bollean Array approach a better one

--- Arrays/2657. Find the Prefix Common Array of Two Arrays.cpp
+++ Arrays/2657. Find the Prefix Common Array of Two Arrays.cpp
@@ -40,3 +40,46 @@ public:
     return result; // Return the result vector containing prefix common element counts
   }
 };
+
+// Approach-2 (Better Approach usign Boolean Array)
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
+class Solution
+{
+public:
+  vector<int> findThePrefixCommonArray(vector<int> &A, vector<int> &B)
+  {
+    int n = A.size(); // Get the size of the input arrays
+
+    vector<int> result(n); // Result vector to store the number of common elements for each prefix
+
+    // Boolean vectors to track the presence of numbers in A and B
+    vector<bool> isPresentA(n + 1, false); // isPresentA[i] = true means i is present in A
+    vector<bool> isPresentB(n + 1, false); // isPresentB[i] = true means i is present in B
+
+    // Iterate through each prefix of the arrays
+    for (int i = 0; i < n; i++)
+    {
+      // Mark the current elements from A and B as present
+      isPresentA[A[i]] = true;
+      isPresentB[B[i]] = true;
+
+      int count = 0; // Counter to track the number of common elements in the current prefix
+
+      // Check for all possible elements from 1 to n
+      for (int num = 1; num <= n; num++)
+      {
+        // If an element is present in both A and B, increment the count
+        if (isPresentA[num] == true && isPresentB[num] == true)
+        {
+          count++;
+        }
+      }
+
+      // Store the count of common elements for the current prefix
+      result[i] = count;
+    }
+
+    return result; // Return the result vector containing prefix common element counts
+  }
+};
